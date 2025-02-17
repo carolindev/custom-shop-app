1e797f7cd530b8cfe31b865080113996a5689fbd
Carolina <carolina@applimarc.com>
2025-02-17 10:58:19 +0100

    Update api config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
 export const PAGE_SIZE = 10;
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://custom-shop-api:8080";
